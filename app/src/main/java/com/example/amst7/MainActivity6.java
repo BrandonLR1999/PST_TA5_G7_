5b7a4003ae6c3f77e07ec6d7caba533d5f2a655f
liselmed <liselmed@espol.edu.ec>
2020-08-12 16:41:03 -0500

    Actualizando activities

--- app/src/main/java/com/example/amst7/MainActivity6.java
+++ app/src/main/java/com/example/amst7/MainActivity6.java
@@ -2,18 +2,39 @@ package com.example.amst7;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity6 extends AppCompatActivity implements View.OnClickListener {
-
+    private TextView nomUsuario;
+    private String nombreUsuario;
+    private TextView name;
+    private TextView lastname;
+    private TextView email;
+    private TextView phone;
+    private TextView fav;
+    AdminSQLiteOpenHelper1 admin=new AdminSQLiteOpenHelper1(this, "datosregistro1",null,1);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main6);
+        nomUsuario = (TextView)findViewById(R.id.textUsuario);
+        Bundle bundle = getIntent().getExtras();
+        nombreUsuario = bundle.getString("usuario".toString());
+        nomUsuario.setText(nombreUsuario);
+        name= (TextView)findViewById(R.id.textView7);
+        lastname= (TextView)findViewById(R.id.textView8);
+        email= (TextView)findViewById(R.id.textView9);
+        phone= (TextView)findViewById(R.id.textView10);
+        fav= (TextView)findViewById(R.id.textView11);
+        //avatarUsuario();
+        mostrarDatos();
         ImageButton exit = (ImageButton)findViewById(R.id.imageButton);
         exit.setOnClickListener((View.OnClickListener)this);
     }
@@ -25,7 +46,7 @@ public class MainActivity6 extends AppCompatActivity implements View.OnClickList
         builder.setNegativeButton("Si", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                finish();
+                finishAffinity();
                 System.exit(0);
             }
         });
@@ -41,4 +62,24 @@ public class MainActivity6 extends AppCompatActivity implements View.OnClickList
     public void avatarUsuario(){
     }
 
+    public void mostrarDatos(){
+        SQLiteDatabase baseDatos = admin.getReadableDatabase();
+        Cursor fila = baseDatos.rawQuery("select nombre,apellido,correo,celular,categoriafav from usuarios1 where usuario = " + "'"+nombreUsuario+"'", null);
+        if (fila.moveToFirst()){
+            name.setText(fila.getString(0));
+            lastname.setText(fila.getString(1));
+            email.setText(fila.getString(2));
+            phone.setText(fila.getString(3));
+            fav.setText(fila.getString(4));
+            baseDatos.close();
+        }else{
+            name.setText("");
+            lastname.setText("");
+            email.setText("");
+            phone.setText("");
+            fav.setText("");
+            baseDatos.close();
+            Toast.makeText(this, "VACIO", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
